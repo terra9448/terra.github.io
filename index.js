4a0b822e9ad078f6eed10889a12040324e85562e
terra9448 <168415630+terra9448@users.noreply.github.com>
2024-04-29 16:16:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,3 +60,29 @@ function filterPropertyByPrice(maxPrice) {
 // Sample Search and Filter
 console.log(searchProperty('City A'));
 console.log(filterPropertyByPrice('$300,000'));
+
+const signUpForm = document.createElement('form');
+signUpForm.setAttribute('id', 'signupForm');
+
+const usernameInput = document.createElement('input');
+usernameInput.setAttribute('type', 'text');
+usernameInput.setAttribute('placeholder', 'Enter your username');
+usernameInput.setAttribute('name', 'username');
+
+const emailInput = document.createElement('input');
+emailInput.setAttribute('type', 'email');
+emailInput.setAttribute('placeholder', 'Enter your email');
+emailInput.setAttribute('name', 'email');
+
+const passwordInput = document.createElement('input');
+passwordInput.setAttribute('type', 'password');
+passwordInput.setAttribute('placeholder', 'Enter your password');
+passwordInput.setAttribute('name', 'password');
+
+const signUpButton = document.createElement('button');
+signUpButton.setAttribute('type', 'submit');
+signUpButton.textContent = 'Sign Up';
+
+signUpForm.append(usernameInput, emailInput, passwordInput, signUpButton);
+
+document.body.appendChild(signUpForm);
